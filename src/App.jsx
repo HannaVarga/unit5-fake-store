2d24a850764ee68904780c87df64888ae26c5c48
HannaVarga <114688311+HannaVarga@users.noreply.github.com>
2023-05-01 16:21:05 +0200

    Commented out hte first line in Appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 import './App.css'
 import Header from './components/Header'
 import Hompage from './pages/Hompage'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <div>
-      
+
       <Header />
       <Hompage />
       <Footer />
