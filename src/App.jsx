515ff2fe0e1a326242c6d7e85f0db971f0db14d6
HannaVarga <114688311+HannaVarga@users.noreply.github.com>
2023-05-02 20:04:28 +0200

    Created a hompage folder in the pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Header from './components/Header'
-import Homepage from './pages/Homepage'
+import Homepage from './pages/Hompage/Homepage'
 import Footer from './components/Footer'
 
 function App() {
