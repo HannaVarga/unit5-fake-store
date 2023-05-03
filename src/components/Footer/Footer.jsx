d4b24ac5130a68bfd68f688f56712fe6dcc34668
HannaVarga <114688311+HannaVarga@users.noreply.github.com>
2023-05-03 17:47:53 +0200

    added styling to the components and pages

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import './Footer.css'
 function Footer() {
   return (
-    <div>
-      Footer
+    <div className='footer-container'>
+      <h4>Made with ❤️ by mimo </h4>
+      <button>Contact us</button>
     </div>
   )
 }
