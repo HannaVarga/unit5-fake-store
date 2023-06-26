d8423ae3f314843b7a8bd414711bd2ad44ecd44e
HannaVarga <hannacamdzij@gmail.com>
2023-06-26 14:49:00 +0200

    updated import in main for context

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ShopContextProvider from "./Context/ShopContext";
+import ShopContextProvider from "../../context/ShopContext";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
